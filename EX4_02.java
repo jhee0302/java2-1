e51159e8528a74e70218cb6b09a914867207fc96
jhee0302 <jhee0302>
2024-04-05 11:36:56 +0900

    import java.util.Scanner;
    public class EX3_13 {
        public static void main(String[] args) {
            Scanner scanner = new Scanner(System.in);
            int dividend; // 나뉨수
            int divisor; // 나눗수
    
            System.out.print("나뉨수를 입력하시오.:"); // 나뉨수 입력
            dividend = scanner.nextInt();
            System.out.print("나눗수를 입력하시오.:"); // 나눗수 입력
            divisor = scanner.nextInt();
            try {
                System.out.println(dividend + "를 " + divisor + "로 나누면 몫은 " + dividend/divisor + "입니다. ");
            }
            catch(ArithmeticException e) { // ArithmeticException 예외 처리 코드
                System.out.println("0으로 나눌 수 없습니다.");
            }
            finally {
                scanner.close(); // 최종적으로 스캐너 닫음
            }
        }
    }

---
+++ EX4_02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Rectangle {
+    int width;
+    int height;
+    public int getArea() {
+        return width*height;
+    }
+}
+
+public class EX4_02 {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle(); // 객체 생성
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(">> ");
+        rect.width = scanner.nextInt();
+        rect.height = scanner.nextInt();
+        System.out.println("사각형의 면적은 " + rect.getArea());
+    }
+}
