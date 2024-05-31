46a96ea0adc224d55655e49742453811ed6d29f3
jhee0302 <0301jhee@gmail.com>
2024-05-31 12:42:22 +0900

    0531

---
+++ LabelEx.java
@@ -0,0 +1,17 @@
+import.javax.swing.*;
+import java.awt.*;
+
+public class LabelEx extends JFrame{
+    public LabelEx() {
+        setTitle("레이블 예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c= getContentPane();
+        c.setLayout(new FlowLayout());
+
+        // 문자열 레이블 생서
+        JLabel textLabel = new JLabel("노무현 입니다.");
+
+        // 이미지 레이블 생성
+        ImageIcon
+    }
+}
